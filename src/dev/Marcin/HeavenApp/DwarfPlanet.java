ff0a30236eff05c5f92d715cb50970fdf366b256
Marcin <gasiorek.mail@gmail.com>
2023-02-20 12:44:37 +0100

    two more subclasses - Moon and DwarfPlanet with constructor.

---
+++ src/dev/Marcin/HeavenApp/DwarfPlanet.java
@@ -0,0 +1,7 @@
+package dev.Marcin.HeavenApp;
+
+public class DwarfPlanet extends HeavenlyBody {
+    public DwarfPlanet(String name, double orbitalPeriod ) {
+        super(name, orbitalPeriod, BodyTypes.DAWRF_PLANET);
+    }
+}
